6d6a5aedf2ee6149ee7609bc31f8ce8ecd1f29aa
Hassan Khan <hassan_farooqkhan@yahoo.com>
2024-05-15 01:36:09 +0500

    Update index.tsx

--- src/components/publication-card/index.tsx
+++ src/components/publication-card/index.tsx
@@ -106,7 +106,7 @@ const PublicationCard = ({
                   {item.description && (
                       
                        <div className="text-center w-full">
-                      <iframe width="350" height="325" src={item.description}>
+                      <iframe width="390" height="325" src={item.description}>
 </iframe>
 
                     </div>
