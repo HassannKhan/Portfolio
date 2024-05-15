78a8da1e1558c153ff46de7ce32999ccba016f2d
Hassan Khan <hassan_farooqkhan@yahoo.com>
2024-05-15 19:16:44 +0500

    Update index.tsx

--- src/components/publication-card/index.tsx
+++ src/components/publication-card/index.tsx
@@ -104,28 +104,12 @@ const PublicationCard = ({
                       Author: {item.authors}
                     </p>
                   )}
-                  .container {
-  position: relative;
-  overflow: hidden;
-  width: 100%;
-  padding-top: 56.25%; /* 16:9 Aspect Ratio (divide 9 by 16 = 0.5625) */
-}
-
-/* Then style the iframe to fit in the container div with full height and width */
-.responsive-iframe {
-  position: absolute;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  width: 100%;
-  height: 100%;
-}
+  
 
                   {item.description && (
                       
-                       <div >
-                      <iframe   width="360" height="325" src={item.description}>
+                       <div class="container">
+                      <iframe  class="responsive-iframe"  width="360" height="325" src={item.description}>
 </iframe>
 
                     </div>
