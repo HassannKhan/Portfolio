f5d6942c8a2196fc9767123048f6cc85aa76b0f7
Hassan Khan <hassan_farooqkhan@yahoo.com>
2024-05-15 18:47:34 +0500

    Update index.tsx

--- src/components/publication-card/index.tsx
+++ src/components/publication-card/index.tsx
@@ -72,7 +72,23 @@ const PublicationCard = ({
 
     return array;
   };
+.container {
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  padding-top: 56.25%; /* 16:9 Aspect Ratio (divide 9 by 16 = 0.5625) */
+}
 
+/* Then style the iframe to fit in the container div with full height and width */
+.responsive-iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+}
   const renderPublications = () => {
     return publications.map((item, index) => (
       <a
@@ -105,8 +121,8 @@ const PublicationCard = ({
                   )}
                   {item.description && (
                       
-                       <div >
-                      <iframe width="360" height="325" src={item.description}>
+                       <div class="container">
+                      <iframe class="responsive-iframe" width="360" height="325" src={item.description}>
 </iframe>
 
                     </div>
